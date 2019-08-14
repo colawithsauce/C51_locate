3a92c37c40e111ce6bf2d47faf0d32de502a4562
colawithsauce <cola_with_sauce@163.com>
2019-08-14 10:33:22 +0800

    test function: UartPrintf()

--- test.c
+++ test.c
@@ -2,19 +2,25 @@
 #include <uart.h>
 
 sbit Key = P1^0;
+sbit Parse = P1^2;
 sbit LED = P0^0;
 
 void delay(unsigned int i);
+void Uart_Init();
 
 void main(){
     Uart_Init();
     delay(1000);
+    
     while (1){
         if (!Key){
-            LED = ~LED
-            UartPrintf("Hello");
-        }else{
-            continue;
+            delay(10000);
+            if(!Key){
+                LED = ~LED;
+                UartPrintf("hello!");
+                }else{
+                    continue;
+                }
         }
     }
 }
