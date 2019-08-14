7accbec363754a3f9994bb4f3f5ce469c66de736
colawithsauce <cola_with_sauce@163.com>
2019-08-14 12:37:28 +0800

    complete parseGpsBuffer(), and haven't complie&test once yet

--- test.c
+++ test.c
@@ -6,11 +6,12 @@ sbit Parse = P1^2;
 sbit LED = P0^0;
 
 void delay(unsigned int i);
-void Uart_Init();
+void errorLog(int num);
 
 void main(){
     Uart_Init();
     delay(1000);
+    clrStruct();
     
     while (1){
         if (!Key){
@@ -21,6 +22,22 @@ void main(){
                 }else{
                     continue;
                 }
+            if(!Parse){
+                delay(10000);
+                if (!Parse){
+
+                    LED = ~LED;
+                    parseGpsBuffer();
+                    UartPrintf("\r\n");
+                    UartPrintf("Done!");
+                    UartPrintf("\r\n");
+                    UartPrintf("test: ");
+                    UartPrintf(SaveData.UTCTime);
+                    UartPrintf("\r\n");
+
+
+                }
+            }
         }
     }
 }
@@ -28,3 +45,71 @@ void main(){
 void delay(unsigned int i){
     while(i--);
 }
+
+void errorLog(int num){
+    while (1)
+    {
+
+        UartPrintf("ERROR");
+        UartPrintASCII(num+0x30);
+        UartPrintf("\r\n");
+
+    }
+}
+
+void parseGpsBuffer(){
+    char *subString;
+    char *subStringNext;
+    char i = 0;
+    if (Save_Data.isGetData)
+    {
+        Save_Data.isGetData = false;
+        UartPrintf("**************\r\n");
+        UartPrintf(Save_Data.GPS_Buffer);
+
+        
+        for (i = 0 ; i <= 6 ; i++)
+        {
+            if (i == 0)
+            {
+                if ((subString = strstr(Save_Data.GPS_Buffer, ",")) == NULL)
+                    errorLog(1);	//解析错误
+            }
+            else
+            {
+                subString++;
+                if ((subStringNext = strstr(subString, ",")) != NULL)
+                {
+                    char usefullBuffer[2]; 
+                    switch(i)
+                    {
+                        case 1:memcpy(Save_Data.UTCTime, subString, subStringNext - subString);break;	//获取UTC时间
+                        case 2:memcpy(usefullBuffer, subString, subStringNext - subString);break;	//获取UTC时间
+                        case 3:memcpy(Save_Data.latitude, subString, subStringNext - subString);break;	//获取纬度信息
+                        case 4:memcpy(Save_Data.N_S, subString, subStringNext - subString);break;	//获取N/S
+                        case 5:memcpy(Save_Data.longitude, subString, subStringNext - subString);break;	//获取经度信息
+                        case 6:memcpy(Save_Data.E_W, subString, subStringNext - subString);break;	//获取E/W
+
+                        default:break;
+                    }
+
+                    subString = subStringNext;
+                    Save_Data.isParseData = true;
+                    if(usefullBuffer[0] == 'A')
+                        Save_Data.isUsefull = true;
+                    else if(usefullBuffer[0] == 'V')
+                        Save_Data.isUsefull = false;
+
+                }
+                else
+                {
+                    errorLog(2);	//解析错误
+                }
+            }
+
+
+        }
+    }
+}
+
+
